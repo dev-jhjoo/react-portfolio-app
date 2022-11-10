9d60f8f5b225ecafcfe2c30327181e40af1e7378
joojh <joojh@apm-coin.com>
2022-11-10 18:55:16 +0900

    project init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Career from './pages/Career';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
+
+function App() {
+  return (<>
+    <h1>JHJOO</h1>
+    <Home></Home>
+    <Resume></Resume>
+    <Career></Career>
+    <Contact></Contact>
+  </>);
+}
+
+export default App;
