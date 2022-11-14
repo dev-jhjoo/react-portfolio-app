d1b9b141b50b4b9c1fccf230dd85b69cfdc1ff82
joojh <joojh@apm-coin.com>
2022-11-14 16:13:05 +0900

    push test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,8 +31,10 @@ function Home(props) {
         roundedCircle={true} 
         style={{width: "150px", height:"150px"}
       }/>
+
       <h3>결제 시스템 개발자</h3>
       <p>Payment Back-End Software Engineer</p>
+      
       <EmptyDiv></EmptyDiv>
       <div style={{width:"100%", height:"30px", fontSize:"20px"}}>
       - Skills -
